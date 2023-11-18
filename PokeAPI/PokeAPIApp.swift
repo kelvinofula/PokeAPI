603da9eaee48f09f445239c90f2f88094e2289b6
Kelvin Ofula <kelvinofula19@gmail.com>
2023-11-18 14:54:19 +0300

    Initial Commit

---
+++ PokeAPI/PokeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAPIApp.swift
+//  PokeAPI
+//
+//  Created by Kelvin Ofula on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct PokeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
